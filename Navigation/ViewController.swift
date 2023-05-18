83e35957e10940fc3d549af1408cc8f3027a6c66
epicure <zolotuhin.maxim@gmail.com>
2023-05-18 09:55:10 +0300

    Initial Commit

---
+++ Navigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Максим Золотухин on 18.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
